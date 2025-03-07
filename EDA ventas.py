df92eb19e86f6aea385b5797986e242a0fd201f5
Yeison De la torre <69448656+yeisondelatorre@users.noreply.github.com>
2025-03-07 12:40:03 -0500

    Update EDA ventas.py
    pestaña para el eda

--- EDA ventas.py
+++ EDA ventas.py
@@ -17,3 +17,22 @@ Los objetivos principales del análisis son:
 
 # Agregar una imagen desde GitHub
 st.image("https://github.com/yeisondelatorre/Trabajorfinal/blob/main/ima1.jpeg?raw=true", caption="Descripción de la imagen", use_column_width=True)
+
+#### pestaña para el eda
+
+# Crear una barra lateral o pestañas para seleccionar entre diferentes secciones
+option = st.sidebar.radio("Selecciona una opción", ["Introducción", "EDA"])
+
+if option == "Introducción":
+    st.subheader("Introducción de la Base de Datos")
+    st.markdown("""
+    Aquí se describe cómo se recogieron los datos, la fuente de la base de datos y otros detalles relevantes.
+    """)
+
+elif option == "EDA":
+    st.subheader("Análisis Exploratorio de Datos (EDA)")
+    st.write("""
+    En esta sección realizaremos el análisis exploratorio de datos (EDA) para comprender mejor la base de datos.
+    Se pueden incluir gráficas, estadísticas descriptivas y otras herramientas útiles para explorar los datos.
+    """)
+    # Aquí es donde agregarías tu código para realizar el EDA más adelante.
